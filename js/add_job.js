d0b2bd3d5044e6d27a8dbbf7e49a3aef16e94bbe
Zack <zklinger@gmail.com>
2015-05-16 13:31:47 -0700

    REMOVE name tag elements and set all id's

---
+++ js/add_job.js
@@ -0,0 +1,6 @@
+// TO DO:  Change "Edit" button to show "Save" when in .editMode
+
+//VARIABLES
+var add_date = document.getElementById("add_date");		//date of the job
+var add_button = document.getElementById("add_button");	//button adds job to temp list
+
